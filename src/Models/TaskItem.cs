ecfbbb981b40012d31ae91b50616230cbc6bfda6
Alice <alicestia@nc-ass-vip.sdv.fr>
2025-11-10 17:13:16 +0100

    Trying to get Step 2 working

--- src/Models/TaskItem.cs
+++ src/Models/TaskItem.cs
@@ -6,5 +6,5 @@ public class TaskItem
 {
     public string Title { get; set; } = string.Empty;
     public bool IsCompleted { get; set; } = false;
-    public string Id { get; set; } = Guid.NewGuid().ToString();
+//    public string Id { get; set; } = Guid.NewGuid().ToString();
 }
